713b8e4bf0d5fbc36e77d9cb32937b78f719374d
Kubing Lin <edkt973@gmail.com>
2023-10-22 23:45:31 +0800

    fix: 新增 RedLock retry 範例

--- Redis.Poc/Services/ConcurrencyService.cs
+++ Redis.Poc/Services/ConcurrencyService.cs
@@ -8,6 +8,7 @@ namespace Redis.Poc.Services
     public class ConcurrencyService : IConcurrencyService
     {
         private const string Key = "concurrency:int_increase";
+        private const string KeyRetry = "concurrency:int_increase_retry";
 
         private readonly IRedisConnection _redisConnection;
         private readonly ILogger<ConcurrencyService> _logger;
@@ -26,9 +27,14 @@ namespace Redis.Poc.Services
         {
             try
             {
-                await RedisDatabase.SetAsync(Key, 0);
+                var expiry = TimeSpan.FromSeconds(30);
+                
+                await RedisDatabase.SetAsync(Key, 0, expiry);
 
                 Parallel.For(0, 50, Increase);
+
+                // await RedisDatabase.SetAsync(KeyRetry, 0, expiry);
+                // Parallel.For(0, 50, IncreaseWithRetry);
             }
             catch (Exception ex)
             {
@@ -41,9 +47,6 @@ namespace Redis.Poc.Services
         private void Increase(int x)
         {
             var expiry = TimeSpan.FromSeconds(30);
-            var wait = TimeSpan.FromSeconds(10);
-            var retry = TimeSpan.FromSeconds(1);
-
             using (var redLock = _redisConnection.RedisLockFactory.CreateLock(Key, expiry))
             {
                 if (redLock.IsAcquired)
@@ -68,6 +71,39 @@ namespace Redis.Poc.Services
                     _logger.LogInformation(
                         message: "Thread: {Thread}, Key: {Key}. Not get the locker",
                         args: new object[] { Thread.CurrentThread.ManagedThreadId, Key });
+                }
+            }
+        }
+
+        private void IncreaseWithRetry(int x)
+        {
+            var expiry = TimeSpan.FromSeconds(30);
+            var wait = TimeSpan.FromSeconds(10);
+            var retry = TimeSpan.FromSeconds(1);
+            using (var redLock = _redisConnection.RedisLockFactory.CreateLock(KeyRetry, expiry, wait, retry))
+            {
+                if (redLock.IsAcquired)
+                {
+                    _logger.LogInformation(
+                        message: "Thread: {Thread}, Key: {Key}. Lock start at {Now}",
+                        args: new object[] { Thread.CurrentThread.ManagedThreadId, KeyRetry, DateTime.Now });
+
+                    var value = RedisDatabase.Get<int>(KeyRetry);
+                    RedisDatabase.Set(KeyRetry, ++value);
+
+                    _logger.LogInformation(
+                        message: "Thread: {Thread}, Key: {Key}. Set value: {Value}",
+                        args: new object[] { Thread.CurrentThread.ManagedThreadId, KeyRetry, value });
+
+                    _logger.LogInformation(
+                        message: "Thread: {Thread}, Key: {Key}. Lock end at {Now}",
+                        args: new object[] { Thread.CurrentThread.ManagedThreadId, KeyRetry, DateTime.Now });
+                }
+                else
+                {
+                    _logger.LogInformation(
+                        message: "Thread: {Thread}, Key: {Key}. Not get the locker",
+                        args: new object[] { Thread.CurrentThread.ManagedThreadId, KeyRetry });
                 }
             }
         }
